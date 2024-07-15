1efe1c0a2e8665deb6583a8042bf5a6922154b8b
igorhollaender <igor.hollaender@gmail.com>
2024-07-15 11:45:51 +0200

    myfirst on RPI

---
+++ myfirst.py
@@ -0,0 +1,52 @@
+# myfirst
+#
+# For Qt, see
+# https://www.pythonguis.com/tutorials/creating-your-first-pyqt-window/
+
+# IH's tips and tricks
+# to activate venv, use (in PowerShell terminal)
+#    Set-ExecutionPolicy AllSigned -Scope CurrentUser
+#    .\.venv\Scripts\activate
+
+# to install packages in virtual env, use something like
+#   (.venv) PS C:\Users\igorh\OneDrive\Dokumente\Igor\UMSAV\Python sandbox>   python -m pip install pyqt5 
+# see https://stackoverflow.com/questions/39630944/python-cant-find-packages-in-virtual-environment
+
+#
+
+# 
+
+from PyQt6.QtWidgets import     \
+        QApplication,           \
+        QWidget,                \
+        QPushButton         
+
+
+# Only needed for access to command line arguments
+import sys
+
+
+
+
+def quit_clicked():
+    QApplication.quit()
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = QWidget()
+
+b = QPushButton("QUIT",window)
+b.clicked.connect(quit_clicked)
+b.setGeometry(1000,100,300,100)
+
+
+# window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+window.showFullScreen()
+
+# Start the event loop.
+app.exec()
+
