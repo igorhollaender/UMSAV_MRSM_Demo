d3254339b115b6aecaeed3e2e25e9f9885a5d77f
Igor Hollaender (BV) <igor.hollaender@gmail.com>
2024-07-17 16:54:45 +0200

    wip

--- code/MRSM_Presentation.py
+++ code/MRSM_Presentation.py
@@ -55,6 +55,8 @@ Demo application to run on the Raspberry Pi MRSM controller:  Display presentati
 #
 #-------------------------------------------------------------------------------
 
+from enum import Enum
+
 from PyQt6.QtWidgets import         \
     QApplication,                   \
     QWidget,                        \
@@ -65,13 +67,78 @@ from PyQt6.QtWidgets import         \
 from MRSM_Globals import IsWaveShareDisplayEmulated
 from MRSM_Globals import IsRaspberryPi5Emulated
 
+class Language(Enum):
+        ENGLISH         = 0
+        SLOVAK          = 1
+        GERMAN          = 2
+
+class PoorMansLocalizer():
+    """
+    Primitive localizer: translates the source English terms and phrases used in GUI
+    to target language
+    """    
+    
+    class MRSM_Dictionary():
+        """
+        ENGLISH to (targetLanguage) TRANSLATIONS
+        """
+        d = [
+            {   'enSrcTerm': 'QUIT',
+                'trsl': [{'tgtLng':  Language.SLOVAK,  'tgtTerm': 'OPUSTIŤ'},
+                         {'tgtLng':  Language.GERMAN,  'tgtTerm': 'VERLASSEN'},
+            ]},
+            {   'enSrcTerm': 'STOP',
+                'trsl': [{'tgtLng':  Language.SLOVAK,  'tgtTerm': 'ZASTAVIŤ'},
+                         {'tgtLng':  Language.GERMAN,  'tgtTerm': 'STEHENBLEIBEN'},
+            ]},
+            {   'enSrcTerm': 'FINISH',
+                'trsl': [{'tgtLng':  Language.SLOVAK,  'tgtTerm': 'SKONČIŤ'},
+                         {'tgtLng':  Language.GERMAN,  'tgtTerm': 'FERTIGMACHEN'},
+            ]},
+        ]
+
+        def __init__(self,targetLanguage: Language) -> None:
+            """
+            Current options for target language include:
+                SLOVAK
+                GERMAN (not fully implemented)
+            """
+            self.targetLanguage = targetLanguage
+        
+        def getTgtTerm(self,srcTerm: str, dictRecord: dict) -> str:
+            if dictRecord['enSrcTerm'] == srcTerm.upper():
+                for langRecord in dictRecord['trsl']:
+                    if langRecord['tgtLng'] == self.targetLanguage:
+                        return langRecord['tgtTerm']
+                return None
+            else:
+                return None
+             
+        def localizeString(self,sourceEnTerm: str):
+            l = list(map(lambda dictRecord: self.getTgtTerm(sourceEnTerm,dictRecord), self.d))
+            print(l)
+            # Get first non None value from List
+            return next((elem for elem in l if elem is not None), None)
+
+    
+    def __init__(self,tgtLanguage: Language) -> None:
+        """
+        """
+        self.dictionary = self.MRSM_Dictionary(tgtLanguage)
+    
+    def localizeString(self,sourceEnTerm: str):
+        return self.dictionary.localizeString(sourceEnTerm)
 
 class MRSM_Presentation():
      
     class MRSM_PushButton(QPushButton):
         def __init_subclass__(cls) -> None:
             return super().__init_subclass__()
-          
+        
+        def __init_subclass__(cls,label,parent) -> None:
+            #TODO IH240717 solve this
+            # return super().__init_subclass__(self.localizer.localizeString(label),parent)
+            return super().__init_subclass__(label,parent)
 
     def __init__(self):
         self.MRSM_Window = QWidget()
@@ -84,6 +151,26 @@ class MRSM_Presentation():
         if IsWaveShareDisplayEmulated:
             self.MRSM_Window.setGeometry(100,200,1480,320)
 
+        #IH240717 for debugging only
+        self.localizer = PoorMansLocalizer(Language.SLOVAK)
+        print(
+            self.localizer.localizeString('QUIT'),
+            self.localizer.localizeString('STOP'),
+            self.localizer.localizeString('stop'),
+            self.localizer.localizeString('FINISH'),
+            self.localizer.localizeString('NONSEnse'),
+        )
+        self.localizer = PoorMansLocalizer(Language.GERMAN)
+        print(
+            self.localizer.localizeString('QUIT'),
+            self.localizer.localizeString('STOP'),
+            self.localizer.localizeString('stop'),
+            self.localizer.localizeString('FINISH'),
+            self.localizer.localizeString('NONSEnse'),
+        )
+
+
+
         # see https://doc.qt.io/qtforpython-6/overviews/stylesheet-examples.html
         self.MRSM_Window.setStyleSheet("QPushButton { background-color: yellow }")
 
@@ -91,17 +178,17 @@ class MRSM_Presentation():
         self.MRSM_Window.setLayout(grid)
      
         #IH240717 for debugging only
-        b1 = self.MRSM_PushButton("QUIT",self.MRSM_Window)
+        b1 = self.MRSM_PushButton(self.localizer.localizeString('QUIT'),self.MRSM_Window)
         b1.clicked.connect(self.quit_clicked)
         grid.addWidget(b1,0,0)
         
         #IH240717 for debugging only
-        b2 = self.MRSM_PushButton("QUIT",self.MRSM_Window)
+        b2 = self.MRSM_PushButton("STOP",self.MRSM_Window)
         b2.clicked.connect(self.quit_clicked)
         grid.addWidget(b2,1,1)
 
         #IH240717 for debugging only
-        b3 = self.MRSM_PushButton("QUIT",self.MRSM_Window)
+        b3 = self.MRSM_PushButton("FINISH",self.MRSM_Window)
         b3.clicked.connect(self.quit_clicked)
         grid.addWidget(b3,2,2)
 
