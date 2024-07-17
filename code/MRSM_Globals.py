7624942d95b9c0e128f59fc2fa6d73974253d8e8
Igor Hollaender (BV) <igor.hollaender@gmail.com>
2024-07-17 15:20:00 +0200

    wip

---
+++ code/MRSM_Globals.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+
+#-------------------------------------------------------------------------------
+#
+#   The Magnetic Resonance Scanner Mockup Project
+#
+#
+#      M  R  S  M  _  G l o b a l s  .  p  y 
+#
+#
+#      Last update: IH240717
+#-------------------------------------------------------------------------------
+
+
+IsWaveShareDisplayEmulated  = True  # set to False for real application
+IsRaspberryPi5Emulated      = True  # set to False for real application
+
