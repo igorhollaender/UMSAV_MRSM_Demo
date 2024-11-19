2385ac8b1483cb43e5c6de7e950756219525f2bd
Igor Holländer <igor.hollaender@savba.sk>
2024-11-19 13:46:24 +0100

    wip

--- code/MRSM_Globals.py
+++ code/MRSM_Globals.py
@@ -10,11 +10,11 @@
 #      M  R  S  M  _  G l o b a l s  .  p  y 
 #
 #
-#      Last update: IH241118
+#      Last update: IH241119
 #-------------------------------------------------------------------------------
 
 
-__version__                 = "MRSM_Demo IH241118a"
+__version__                 = "MRSM_Demo IH241119a"
 
 IsWaveShareDisplayEmulated  = True   # set to False for real application
 IsRaspberryPi5Emulated      = True   # set to False for real application
